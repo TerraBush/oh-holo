c115584011f53fbba482a0859c83b49b78d420f1
TerraBush <redrobin0611@gmail.com>
2024-02-28 17:41:31 -0500

    main

--- main.js
+++ main.js
@@ -1,27 +1,3 @@
-/*const ah = {
-    onClick() {
-        const e = qr(1,3);
-        let t = e.toString();
-        const r = new Audio('sounds/hi-${t}.mp3');
-        r.volume = .7,
-        r.play()
-    }
-}*/
-/*document.addEventListener("DOMContentLoaded", function() {
-
-    var image = document.getElementById("clickableImage");
-
-    function playNoise() {
-        
-        const e = qr(1,3);
-        let t = e.toString();
-        const r = new Audio('sounds/hi-${t}.mp3');
-        r.volume = .7;
-        r.play();
-    }
-
-    image.addEventListener("click", playNoise);
-});*/
 document.addEventListener("DOMContentLoaded", function() {
 
     var image = document.getElementById("clickableImage");
