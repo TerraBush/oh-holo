44750cf2bcc8091a5eb11dfe4f8490f30ea6b275
TerraBush <redrobin0611@gmail.com>
2024-03-07 19:35:36 -0500

    livestream display item order change

--- fetch-stats.js
+++ fetch-stats.js
@@ -47,11 +47,9 @@ function lastestLivestream() {
 
             document.getElementById('livestreamDisplay').innerHTML = `
                 <img src="${thumbnailUrlHigh}" class="card-image" alt="Image">
-                <div class="card-overlay">
-                    <p class="top-left-text">${title}</p>
-                    <p class="bottom-left-text">${localDateTime}</p>
-                    <a href="${videoUrl}" class="button">Watch</a>
-                </div>
+                <p class="top-left-text">${title}</p>
+                <p class="bottom-left-text">${localDateTime}</p>
+                <a href="${videoUrl}" class="button">Watch</a>
             `;
         })
         .catch(error => {
