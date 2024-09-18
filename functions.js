fb6f4f19c38e84b90d5feed1ed796aa671ecd95d
TerraBush <redrobin0611@gmail.com>
2024-09-18 15:22:28 -0400

    added setCookie to getCookie if there is no cookie yet

--- functions.js
+++ functions.js
@@ -167,7 +167,7 @@ function updateDisplay(videoType) {
         document.getElementById("videoThumbnailLink").href = `${liveUrl}`;
         document.getElementById("dateDisplay").innerHTML = `${liveDate}`;
     } else if (videoType == "completed") {
-        document.getElementById("titleDisplay").innerHTML = `${premiereEmote}${premiereTitle}`;
+        document.getElementById("titleDisplay").innerHTML = `${completedEmote}${completedTitle}`;
         document.getElementById("videoThumbnail").src = `${completedThumbnail}`;
         document.getElementById("videoThumbnailLink").href = `${completedUrl}`;
         document.getElementById("dateDisplay").innerHTML = `${completedDate}`;
@@ -189,7 +189,8 @@ function getCookie(name) {
             return decodeURIComponent(cookie.substring(name.length + 1));
         }
     }
-    return "abcdefg";
+    setCookie(name, null, 365);
+    return null;
 }
 function updateButtonDisplay() {
     if(getCookie("premiereUrl") == null){
