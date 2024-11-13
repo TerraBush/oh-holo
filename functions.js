21082803d648b19e027a99a192df76c3ec76000f
TerraBush <redrobin0611@gmail.com>
2024-11-12 20:44:23 -0500

    attempted to fix click problem by escaping quotes

--- functions.js
+++ functions.js
@@ -854,7 +854,7 @@ function updateStatusDisplay() {
     let display = document.getElementById("statusDisplay");
     display.innerHTML = "";
 
-    display.insertAdjacentHTML("beforeend", `<p onclick="switchDropdown("${channelNameList[0]}")">test</p><hr>`);
+    display.insertAdjacentHTML("beforeend", `<p onclick="switchDropdown(\"${channelNameList[0]}\")">test</p><hr>`);
 
     for(let i = 0; i < channelNameList.length; i++) {
         //display.insertAdjacentHTML("beforeend", `<p>${channelNameList[i]}</p>`);
